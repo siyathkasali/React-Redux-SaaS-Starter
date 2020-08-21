f1107a9980c98dde017ff3a72402ae22c601897e
Siyath Kasali <jsiyath@gmail.com>
2020-08-21 23:45:05 +0530

    added sass loader

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
         use: 'babel-loader',
       },
       {
-        test: /\.(css)/, use: ['style-loader', 'css-loader'],
+        test: /\.(scss)/, use: ['style-loader', 'css-loader', 'sass-loader'],
       },
     ],
   },
